9c43ef55568d292534de77f757d9be54a3d539cb
maurycy1996 <maurycy1996@gmail.com>
2023-04-26 16:13:43 +0200

    v1

--- 00_Advent of Code/2022/02_Rock Paper Scissors.py
+++ 00_Advent of Code/2022/02_Rock Paper Scissors.py
@@ -1,7 +1,47 @@
+# %% Creating list of rounds from txt file
+
+match = []
+
+with open("02_input.txt", "r") as file:
+    for line in file:
+        round = []
+        for i in line:
+            if i.isalpha():
+                round.append(i)
+        match.append(round)
+
+match
+    
 # %%
 
-with open("00_Advent of Code/2022/02_input.txt", "r") as file:
-    for i in file:
-        print(i)
-        
+def pts(x, y):
+    
+    wins = [["X", "C"], ["Y", "A"], ["Z", "B"]]
+    
+    shp = 0
+    res = 0
+    
+    if y == "X":
+        shp = 1
+    elif y == "Y":
+        shp = 2
+    elif y == "Z":
+        shp = 3
+    
+    if x == y:
+        res = 3
+    elif [y, x] in wins:
+        res = 6
+    else:
+        res = 0 
+
+    return shp
+    
+# %%
+
+for round in match:
+    points = pts(round[0], round[1])
+    print(round[0] == round[1])
+
+
 # %%
