4757c21ca14c8215c0a30cfc00bd9c769cc9803c
maurycy1996 <maurycy1996@gmail.com>
2023-04-26 14:58:21 +0200

    01_Calorie Counting - solution

--- 00_Advent of Code/2022/01_Calorie Counting.py
+++ 00_Advent of Code/2022/01_Calorie Counting.py
@@ -1,11 +1,26 @@
 # %%
 cal = []
 
-with open("01_calories.txt") as file:
+with open("00_Advent of Code/2022/01_calories.txt", "r") as file:
     for i in file:
+        i = i[:-1]
         cal.append(i)
 
-cal
+# %%
 
+total_cal = []
+per_elf = 0
 
+for j in cal:
+    if j:
+        j = int(j)
+        per_elf += j
+    else:
+        total_cal.append(per_elf)
+        per_elf = 0
+total_cal.append(per_elf)
+  
+print(f"The calory-richest elf has a total of {max(total_cal)} calories on the"\
+      " expedition.")
+        
 # %%
